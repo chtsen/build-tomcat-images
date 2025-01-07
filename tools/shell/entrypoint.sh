c6ffb16cd0c106c6bea6f15a761d8d85046b2acc
chtsen <46036990+chtsen@users.noreply.github.com>
2025-01-07 13:46:57 +0800

    Update entrypoint.sh

--- tools/shell/entrypoint.sh
+++ tools/shell/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-/opt/yozo/bin/gotpl ${CATALINA_HOME}/conf/server.xml.tpl
-/opt/yozo/bin/gotpl ${CATALINA_HOME}/webapps/fcscloud/WEB-INF/classes/application.yml.tpl
-/opt/yozo/bin/gotpl ${CATALINA_HOME}/webapps/fcscloud/WEB-INF/classes/config.properties.tpl
-/opt/yozo/bin/gotpl ${CATALINA_HOME}/webapps/fcscloud/WEB-INF/classes/bootstrap.yml.tpl
-/opt/yozo/bin/gotpl ${CATALINA_HOME}/webapps/fcscloud/WEB-INF/classes/convertConfig.properties.tpl
-
-catalina.sh run
+/opt/yozo/fcscloud/bin/catalina.sh run
