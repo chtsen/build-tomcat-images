409722e6bc74d4765b5a0353b1d5492c75e54d8a
chtsen <46036990+chtsen@users.noreply.github.com>
2025-01-09 10:28:25 +0800

    Update entrypoint.sh

--- tools/shell/yozo-tomcat/entrypoint.sh
+++ tools/shell/yozo-tomcat/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/opt/yozo/bin/gotpl /opt/yozo/fcscloud/conf/server.xml.tpl
-/opt/yozo/bin/gotpl /opt/yozo/fcscloud/webapps/fcscloud/WEB-INF/classes/application.yml.tpl
-/opt/yozo/bin/gotpl /opt/yozo/fcscloud/webapps/fcscloud/WEB-INF/classes/config.properties.tpl
-/opt/yozo/bin/gotpl /opt/yozo/fcscloud/webapps/fcscloud/WEB-INF/classes/bootstrap.yml.tpl
-/opt/yozo/bin/gotpl /opt/yozo/fcscloud/webapps/fcscloud/WEB-INF/classes/convertConfig.properties.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/conf/server.xml.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/application.yml.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/config.properties.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/bootstrap.yml.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/convertConfig.properties.tpl
 
 /opt/yozo/fcscloud/bin/catalina.sh run
