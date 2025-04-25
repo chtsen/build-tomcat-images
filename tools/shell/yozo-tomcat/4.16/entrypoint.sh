d2b11065c37c3a2f3a9e98926aa6e9a73f528def
chtsen <46036990+chtsen@users.noreply.github.com>
2025-04-25 15:38:04 +0800

    Create entrypoint.sh

---
+++ tools/shell/yozo-tomcat/4.16/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/conf/server.xml.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/application.yml.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/config.properties.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/bootstrap.yml.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/convertConfig.properties.tpl
+
+/opt/yozo/fcscloud_tomcat/bin/catalina.sh run
