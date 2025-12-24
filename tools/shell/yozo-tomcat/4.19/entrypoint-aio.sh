d132cbaea5c02a0554c7c334fc09dcfbef57650f
chtsen <46036990+chtsen@users.noreply.github.com>
2025-12-24 15:38:47 +0800

    Create entrypoint-aio.sh

---
+++ tools/shell/yozo-tomcat/4.19/entrypoint-aio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/conf/server.xml.tpl
+/opt/yozo/bin/gotpl /opt/yozo/fcscloud_tomcat/webapps/fcscloud/WEB-INF/classes/application-prod.yml.tpl
+
+# 需要检查创建的目录列表
+directories=( "/opt/yozo/data/redis" "/opt/yozo/data/logs/redis" )
+ 
+# 循环检查并创建每个目录
+for dir in "${directories[@]}"; do
+  if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
+    echo "Directory $dir created."
+  else
+    echo "Directory $dir already exists."
+  fi
+done
+
+/usr/bin/redis-server /opt/yozo/conf/redis/redis.conf 
+/opt/yozo/fcscloud_tomcat/bin/catalina.sh run
